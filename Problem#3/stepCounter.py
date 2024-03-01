1ca175506ed68e18b9b9d32b8d92b33ee33d4ce1
Will Morris <wdm9727@utulsa.edu>
2024-03-01 12:20:02 -0600

    added indent

--- Problem#3/stepCounter.py
+++
@@ -1,8 +0,0 @@
-def feet_to_steps(user_feet):
-   #write your code here
-
-if __name__ == '__main__':
-    #take input feet steps here
-    #store it into the function
-    
-    #print your steps here
--- problem#4/intToBin.py
+++ problem#4/intToBin.py
@@ -4,7 +4,7 @@ def int_to_reverse_binary(num1):
     while num1 > 0:
         #write your code
 
-    return binary_val;
+        return binary_val
 
 
 def string_reverse(input_string): 
