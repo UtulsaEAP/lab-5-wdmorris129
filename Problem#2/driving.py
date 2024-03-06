f5794035716b570fdf711b7ee241ac2f122c6824
Will Morris <wdm9727@utulsa.edu>
2024-03-06 17:52:34 -0600

    edited code for problem 2

--- Problem#2/driving.py
+++ Problem#2/driving.py
@@ -7,4 +7,5 @@ dollars_per_gallon = float(input())
 
 if __name__ == '__main__':
    print(f'{driving_cost(miles_per_gallon, dollars_per_gallon, 10.0):.2f}')
-   
+   print(f'{driving_cost(miles_per_gallon, dollars_per_gallon, 50.0):.2f}')
+   print(f'{driving_cost(miles_per_gallon, dollars_per_gallon, 400.0):.2f}')
