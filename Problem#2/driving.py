e5f42fdfc98ece25e8f5c12049c1a2e3668f146c
Will Morris <wdm9727@utulsa.edu>
2024-03-06 18:02:36 -0600

    trying to run pytest

---
+++ Problem#2/driving.py
@@ -0,0 +1,20 @@
+def driving_cost(miles_per_gallon, dollars_per_gallon, miles_driven):
+   cost = miles_driven / miles_per_gallon * dollars_per_gallon
+
+
+
+
+if __name__ == '__main__':
+   miles_per_gallon = float(input())
+   dollars_per_gallon = float(input())
+
+
+
+
+# print(f'{driving_cost(miles_per_gallon, dollars_per_gallon, 10.0):.2f}')
+# print(f'{driving_cost(miles_per_gallon, dollars_per_gallon, 50.0):.2f}')
+# print(f'{driving_cost(miles_per_gallon, dollars_per_gallon, 400.0):.2f}')
+   
+      # return cost
+   
+   
